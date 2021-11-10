ba5b64e762b94502ba8708f8518827b4b8c197a6
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2021-11-11 00:56:28 +0900

    feat handling carlist input

--- java-racingcar-precourse/src/main/java/racingcar/Process.java
+++ java-racingcar-precourse/src/main/java/racingcar/Process.java
@@ -14,6 +14,27 @@ public class Process {
 
     public void start(){
         this.newGame = new Game();
+        getCar();
+    }
+
+    private void getCar(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String userInput = scanner.next();
+        try {
+            new InputUtils(userInput).checkUserCarInput();
+            saveCarList(userInput);
+        }catch(IllegalArgumentException e) {
+            System.out.println("[ERROR] 자동차 이름은 5자 이하만 가능합니다");
+            getCar();
+        }
+    }
+
+    private void saveCarList(String cars){
+
+    }
+
+    private void getTimes(){
+
     }
 
 }
--- java-racingcar-precourse/src/main/java/utils/InputUtils.java
+++ java-racingcar-precourse/src/main/java/utils/InputUtils.java
@@ -3,5 +3,25 @@ package utils;
 import java.util.ArrayList;
 
 public class InputUtils {
+    String userInput;
 
+    public InputUtils(String userInput) {
+        this.userInput = userInput;
+    }
+
+    public void checkUserCarInput() throws IllegalArgumentException{
+        String[] carList = userInput.split(",");
+        for (String i : carList){
+            if(!isCarLength(i)){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+    private boolean isCarLength(String car){
+        int carLength = car.length();
+        if (carLength > 5 || carLength == 0){
+            return false;
+        }
+        return true;
+    }
 }
